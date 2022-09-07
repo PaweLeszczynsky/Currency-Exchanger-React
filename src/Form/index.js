4599638d89d02692b24739ded1cd5bfbb3ba2677
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-09-07 20:03:30 +0200

    Form component added

---
+++ src/Form/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+const Form = () => {
+    <form className="exchangerForm">
+        <fieldset className="exchangerForm__fieldset">
+            
+        </fieldset>
+    </form>
+};
+export default Form;
