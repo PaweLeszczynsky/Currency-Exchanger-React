ad2388a6ef34b7b971606832d17b9deec817eba0
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-09-07 20:46:30 +0200

    added legend in Form component and styles

--- src/Form/index.js
+++ src/Form/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Form = () => {
     <form className="exchangerForm">
         <fieldset className="exchangerForm__fieldset">
-            
+            <legend className="exchangerForm__legend">Currency Exchanger</legend>
         </fieldset>
     </form>
 };
