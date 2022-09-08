5600817e2901bf977bbfd04708eb6fc0fe76122f
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-09-08 19:41:03 +0200

    added input to Select component

--- src/Select/index.js
+++ src/Select/index.js
@@ -1,12 +1,16 @@
 import "./style.css";
 
-const Select = ({name}) => {
-    <select className="exchangerForm__selectCurrency" name={name}>
-        <option value="PLN" selected>PLN</option>
-        <option value="EURO">EURO</option>
-        <option value="USD">USD</option>
-        <option value="GBP">GBP</option>
-        <option value="CHF">CHF</option>
-    </select>
+const Select = ({ name }) => {
+    <>
+        <select className="exchangerForm__selectCurrency" name={name}>
+            <option value="PLN" selected>PLN</option>
+            <option value="EURO">EURO</option>
+            <option value="USD">USD</option>
+            <option value="GBP">GBP</option>
+            <option value="CHF">CHF</option>
+        </select>
+        <input className="exchangerForm__amountCurrency"
+            name="amountToexchange" type="number" min="0" step="0.01" />
+    </>
 };
 export default Select;
