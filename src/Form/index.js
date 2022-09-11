5e642a61c7aaca35284e0a14ef301ebabeeca5d8
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-09-11 13:11:42 +0200

    onInput event added in Form component

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,7 +1,11 @@
 import "./style.css";
-
+const onFormSubmit = (event) => {
+    event.preventDefault();
+};
 const Form = ({ firstItem, secoundItem, thirdItem, fourthItem }) => (
-    <form className="exchangerForm">
+
+    <form onInput={onFormSubmit}
+        className="exchangerForm">
         <fieldset className="exchangerForm__fieldset">
             <legend className="exchangerForm__legend">Currency Exchanger</legend>
             {firstItem}
@@ -11,4 +15,5 @@ const Form = ({ firstItem, secoundItem, thirdItem, fourthItem }) => (
         </fieldset>
     </form>
 );
+
 export default Form;
