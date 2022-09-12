d91be708e5423fd3c5632fcc2a4370bdca522a77
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-09-12 19:53:11 +0200

    created file with allcurrencies array

---
+++ src/currenciesArray.js
@@ -0,0 +1,7 @@
+export const allCurrencies = [
+    { id: 1, name: "PLN", value: 1 },
+    { id: 2, name: "EURO", value: 0.2099 },
+    { id: 3, name: "USD", value: 0.2194 },
+    { id: 4, name: "CHF", value: 0.2131 },
+    { id: 5, name: "GBP", value: 0.1789 },
+];
