f1e1e89fadb3d3d9ba67a272e65c189a8cdd55c0
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-09-13 17:51:50 +0200

    added condition to make visible component

--- src/SummaryInfo/index.js
+++ src/SummaryInfo/index.js
@@ -1,6 +1,11 @@
 import "./style.css";
-const SummaryInfo = () => (
-    <div className="exchangerForm__summaryExchange">
-    </div>
-);
+const SummaryInfo = ({ currencies }) => {
+    return (
+        <div className={`exchangerForm__summaryExchange ${currencies.toExchangeAmount > 0 ? "exchangerForm__summaryExchange--visible" : ""}`}>
+            {currencies.firstCurrency === currencies.secoundCurrency ?
+                `Choose another currency` :
+                (`${currencies.toExchangeAmount} ${currencies.firstCurrency} = ${currencies.exchangedAmount} ${currencies.secoundCurrency}`)}
+        </div>
+    )
+};
 export default SummaryInfo;
