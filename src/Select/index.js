5942fbc4aaf03f9eed04603ef45bd1f1f657609b
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-09-16 20:21:41 +0200

    props changes in component, added function to display result in input

--- src/Select/index.js
+++ src/Select/index.js
@@ -1,34 +1,37 @@
 import "./style.css";
 import { allCurrencies } from "../currenciesArray";
 
-const Select = ({ currencies, setCurrencies, selectName, selectId, inputName, disabledValue, stepValue }) => {
+const Select = ({ result,firstCurrency, setFirstCurrency, secoundCurrency, setSecoundCurrency, amountToExchange, setAmountToExchange, selectName, selectId, inputName, disabledValue, stepValue }) => {
     const findSelect = (index) => {
-        return (index === 1 ? currencies.firstCurrency : currencies.secoundCurrency)
+        return (index === 1 ? firstCurrency : secoundCurrency)
     };
-
     const setSecoundCurency = (index, target) => {
         return (
             index === 1 ?
-                setCurrencies({ ...currencies, firstCurrency: target.value })
-                : setCurrencies({ ...currencies, secoundCurrency: target.value }))
+                setFirstCurrency(target.value)
+                : setSecoundCurrency(target.value))
     };
-
+    const setInput=({target})=>{
+        setAmountToExchange(target.value)
+    };
+    const findValue =(index)=>{
+        return (index === 1 ? amountToExchange : result.toFixed(2))
+    };
     return (
-
         <>
             <select className="exchangerForm__selectCurrency" name={selectName} value={findSelect(selectId)}
                 onChange={({ target }) => setSecoundCurency(selectId, target)}
             >
                 {
-                    allCurrencies?.map(({ id, name }) => (
+                    allCurrencies.map(({ id, name }) => (
                         <option key={id} value={name}>{name}</option>)
                     )
                 }
             </select>
             <input className="exchangerForm__amountCurrency"
                 name={inputName}
-                disabled={disabledValue} type="number" min="0" step={stepValue}
-                onChange={({ target }) => setCurrencies({ ...currencies, toExchangeAmount: target.value })}
+                disabled={disabledValue} type="number" min="0" step={stepValue} value={findValue(selectId)}
+                onChange={setInput}
             />
         </>
     )
