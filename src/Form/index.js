94a5ccc63908d424bc0cbe499f8071d43dc5f1d8
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-09-16 20:25:43 +0200

    props changes, added functions to calculate rate and result

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,19 +1,24 @@
 import "./style.css";
-const onFormSubmit = (event) => {
-    event.preventDefault();
-};
-const Form = ({ firstItem, secoundItem, thirdItem, fourthItem }) => (
 
-    <form onInput={onFormSubmit}
-        className="exchangerForm">
-        <fieldset className="exchangerForm__fieldset">
-            <legend className="exchangerForm__legend">Currency Exchanger</legend>
-            {firstItem}
-            {secoundItem}
-            {thirdItem}
-            {fourthItem}
-        </fieldset>
-    </form>
-);
+const Form = ({ calculateRate, calculateResult, firstCurrency, secoundCurrency, amountToExchange, rate, firstItem, secoundItem, thirdItem, fourthItem }) => {
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(firstCurrency, secoundCurrency, amountToExchange, rate);
+        calculateRate(firstCurrency, secoundCurrency);
+        console.log(firstCurrency, secoundCurrency, amountToExchange, rate);
+        calculateResult(amountToExchange, rate);
+    };
+    return (
+        <form onInput={onFormSubmit}
+            className="exchangerForm">
+            <fieldset className="exchangerForm__fieldset">
+                <legend className="exchangerForm__legend">Currency Exchanger</legend>
+                {firstItem}
+                {secoundItem}
+                {thirdItem}
+                {fourthItem}
+            </fieldset>
+        </form>)
+};
 
 export default Form;
