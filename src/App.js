30edae147c0dd7951e748915ca68d982a33f6e66
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-09-24 18:41:28 +0200

    format code, move components from App to Form

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import Container from "./Container";
-import InputContainer from "./InputContainer";
-import Form from "./Form";
-import Select from "./Select";
+import Form from "./Container/Form";
 import { useState, useEffect } from "react";
-import LabelRate from "./LabelRate";
-import SummaryInfo from "./SummaryInfo";
 import { allCurrencies } from "./currenciesArray";
+
 function App() {
   const [firstCurrency, setFirstCurrency] = useState(allCurrencies[0].name);
   const [secoundCurrency, setSecoundCurrency] = useState(allCurrencies[0].name);
   const [amountToExchange, setAmountToExchange] = useState(0);
   const [rate, setRate] = useState(1);
   const [result, setResult] = useState(0);
+
   useEffect(() => {
     const firstValue = allCurrencies.find(({ name }) => name === firstCurrency).value;
     const secoundValue = allCurrencies.find(({ name }) => name === secoundCurrency).value;
     const rateValue = secoundValue / firstValue;
     setRate(rateValue);
   }, [firstCurrency, secoundCurrency]);
+
   useEffect(() => {
     if (amountToExchange < 0) {
       setAmountToExchange(Math.abs(amountToExchange));
     };
     setResult(rate * amountToExchange);
   }, [rate, amountToExchange]);
+  
   return (
     <Container>
       <Form
@@ -32,59 +32,9 @@ function App() {
         amountToExchange={amountToExchange}
         rate={rate}
         result={result}
-        firstItem={
-          <InputContainer
-            body={
-              <Select
-                firstCurrency={firstCurrency}
-                secoundCurrency={secoundCurrency}
-                amountToExchange={amountToExchange}
-                rate={rate}
-                result={result}
-                setFirstCurrency={setFirstCurrency}
-                setAmountToExchange={setAmountToExchange}
-                selectName="firstCurrency"
-                selectId={1}
-                inputName="secoundCurrency"
-                disabledValue={false}
-                stepValue={0.01}
-              />}
-          />}
-        secoundItem={
-          <InputContainer
-            body={
-              <Select
-                firstCurrency={firstCurrency}
-                secoundCurrency={secoundCurrency}
-                amountToExchange={amountToExchange}
-                rate={rate}
-                setSecoundCurrency={setSecoundCurrency}
-                setAmountToExchange={setAmountToExchange}
-                selectName="secoundCurrency"
-                selectId={2}
-                inputName="amountExchanged"
-                disabledValue={true}
-                result={result}
-              />}
-          />}
-        thirdItem={
-          <InputContainer
-            body={
-              <LabelRate
-                firstCurrency={firstCurrency}
-                secoundCurrency={secoundCurrency}
-                amountToExchange={amountToExchange}
-                result={result}
-                rate={rate}
-              />}
-          />}
-        fourthItem={
-          <SummaryInfo
-            firstCurrency={firstCurrency} rate={rate}
-            secoundCurrency={secoundCurrency}
-            amountToExchange={amountToExchange}
-            result={result}
-          />}
+        setFirstCurrency={setFirstCurrency}
+        setSecoundCurrency={setSecoundCurrency}
+        setAmountToExchange={setAmountToExchange}
       />
     </Container>
   );
