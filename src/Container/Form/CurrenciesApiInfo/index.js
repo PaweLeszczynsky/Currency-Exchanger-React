0ccaa4a5dd42f42ed1fd4fea9b5be0c0e43431d5
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-10-29 15:59:25 +0200

    changes in component, added props which contain date of downloaded API

--- src/Container/Form/CurrenciesApiInfo/index.js
+++ src/Container/Form/CurrenciesApiInfo/index.js
@@ -1,12 +1,10 @@
-import { useCurrenciesArray } from "../../../currencies";
 import { StyledUpdatedCurencies } from "./styled";
 
-export const CurrenciesApiInfo = () => {
+export const CurrenciesApiInfo = ({ currenciesDate }) => {
 
-    const {currenciesArray, mapedCurrenciesArray, currenciesDate, updateApiStatus} = useCurrenciesArray();
     return (
         <StyledUpdatedCurencies>
-            Currency data delivered are sourced from financial data providers and banks, including the European Central Bank.<br/>
+            Currency data delivered are sourced from financial data providers and banks, including the European Central Bank.<br />
             Updated as: <b>{currenciesDate}</b>
         </StyledUpdatedCurencies>
     );
