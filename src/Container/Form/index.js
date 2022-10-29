6ef510534e937ef3b864fbc6bd53db8201b03a2b
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-10-29 16:21:17 +0200

    added new components and API data, changes in code-render components depending on updateApiStatus

--- src/Container/Form/index.js
+++ src/Container/Form/index.js
@@ -2,58 +2,75 @@ import Select from "./Select";
 import LabelRate from "./LabelRate";
 import SummaryInfo from "./SummaryInfo";
 import DateContainer from "./DateContainer";
+import { CurrenciesApiInfo } from "./CurrenciesApiInfo";
 import { StyledForm, StyledFieldset, StyledLegend } from "./styled";
+import { UpdatingContainer } from "./UpdatingContainer";
+import { ErrorContainer } from "./ErrorContainer";
 
-const Form = ({ date, firstCurrency, secoundCurrency, amountToExchange,
-    rate, result, setFirstCurrency, setSecoundCurrency,
-    setAmountToExchange }) => {
+const Form = ({ firstCurrency, secoundCurrency, amountToExchange,
+    setFirstCurrency, setSecoundCurrency, result,
+    setAmountToExchange, currenciesArray, mapedCurrenciesArray,
+    currenciesDate, updateApiStatus, rate }) => {
+
     return (
         <StyledForm>
             <StyledFieldset>
                 <StyledLegend>
                     Currency Exchanger
                 </StyledLegend>
-                <DateContainer date={date} />
-                    <Select
-                        firstCurrency={firstCurrency}
-                        secoundCurrency={secoundCurrency}
-                        amountToExchange={amountToExchange}
-                        rate={rate}
-                        result={result}
-                        setFirstCurrency={setFirstCurrency}
-                        setAmountToExchange={setAmountToExchange}
-                        selectName="firstCurrency"
-                        selectId={1}
-                        inputName="secoundCurrency"
-                        disabledValue={false}
-                        stepValue={0.01}
-                    />
-                    <Select
-                        firstCurrency={firstCurrency}
-                        secoundCurrency={secoundCurrency}
-                        amountToExchange={amountToExchange}
-                        rate={rate}
-                        setSecoundCurrency={setSecoundCurrency}
-                        setAmountToExchange={setAmountToExchange}
-                        selectName="secoundCurrency"
-                        selectId={2}
-                        inputName="amountExchanged"
-                        disabledValue={true}
-                        result={result}
-                    />
-                    <LabelRate
-                        firstCurrency={firstCurrency}
-                        secoundCurrency={secoundCurrency}
-                        amountToExchange={amountToExchange}
-                        result={result}
-                        rate={rate}
-                    />
-                <SummaryInfo
-                    firstCurrency={firstCurrency} rate={rate}
-                    secoundCurrency={secoundCurrency}
-                    amountToExchange={amountToExchange}
-                    result={result}
-                />
+                <DateContainer />
+                {updateApiStatus === "loading" && (
+                    <UpdatingContainer />
+                )}
+                {updateApiStatus === "updated" && (
+                    <>
+                        <Select
+                            firstCurrency={firstCurrency}
+                            secoundCurrency={secoundCurrency}
+                            amountToExchange={amountToExchange}
+                            result={result}
+                            setFirstCurrency={setFirstCurrency}
+                            setAmountToExchange={setAmountToExchange}
+                            selectName="firstCurrency"
+                            selectId={1}
+                            inputName="secoundCurrency"
+                            disabledValue={false}
+                            stepValue={0.01}
+                            mapedCurrenciesArray={mapedCurrenciesArray}
+                        />
+                        <Select
+                            firstCurrency={firstCurrency}
+                            secoundCurrency={secoundCurrency}
+                            amountToExchange={amountToExchange}
+                            rate={rate}
+                            setSecoundCurrency={setSecoundCurrency}
+                            setAmountToExchange={setAmountToExchange}
+                            selectName="secoundCurrency"
+                            selectId={2}
+                            inputName="amountExchanged"
+                            disabledValue={true}
+                            result={result}
+                            mapedCurrenciesArray={mapedCurrenciesArray}
+                        />
+                        <LabelRate
+                            firstCurrency={firstCurrency}
+                            secoundCurrency={secoundCurrency}
+                            amountToExchange={amountToExchange}
+                            result={result}
+                            rate={rate}
+                        />
+                        <SummaryInfo
+                            firstCurrency={firstCurrency} rate={rate}
+                            secoundCurrency={secoundCurrency}
+                            amountToExchange={amountToExchange}
+                            result={result}
+                        />
+                        <CurrenciesApiInfo currenciesDate={currenciesDate} />
+                    </>
+                )}
+                {updateApiStatus === "error" && (
+                    <ErrorContainer />
+                )}
             </StyledFieldset>
         </StyledForm>)
 };
